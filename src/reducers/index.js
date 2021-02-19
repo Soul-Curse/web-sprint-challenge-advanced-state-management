bce54ae460856952d532f45ebf0a1e1dad473c6f
Daniel Wright <daniel.wright1207@gmail.com>
2021-02-19 11:13:26 -0700

    final after flex

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,9 +1,65 @@
+import {
+  FETCH_SMURF_START,
+  FETCH_SMURF_SUCCESS,
+  FETCH_SMURF_FAIL,
+  ADD_SMURF,
+  ADD_SMURF_START,
+  POST_ERROR,
+  SUBMIT_ERROR,
+} from "./../actions/index";
 
 export const initialState = {
-}
+  smurfs: [],
+  isLoading: false,
+  error: "",
+};
 
-const reducer = ()=>{
-}
+export const reducer = (state = initialState, action) => {
+  switch (action.type) {
+    case FETCH_SMURF_START:
+      return {
+        ...state,
+        isLoading: true,
+        smurfs: [],
+      };
+    case FETCH_SMURF_SUCCESS:
+      return {
+        ...state,
+        smurfs: action.payload,
+        isLoading: false,
+        error: "",
+      };
+    case FETCH_SMURF_FAIL:
+      return {
+        ...state,
+        error: action.payload,
+        isLoading: false,
+      };
+    case ADD_SMURF:
+      return {
+        ...state,
+        smurfs: action.payload,
+        error: "",
+        isLoading: false,
+      };
+    case ADD_SMURF_START:
+      return state;
+    case POST_ERROR:
+      return {
+        ...state,
+        error: action.payload,
+        isLoading: false,
+      };
+    case SUBMIT_ERROR:
+      return {
+        ...state,
+        error: action.payload,
+        isLoading: false,
+      };
+    default:
+      return state;
+  }
+};
 
 export default reducer;
 
